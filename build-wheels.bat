f4841b27b9805ffb3511820e56f4022f94055f4f
Ulf Magnusson <ulfalizer@gmail.com>
2018-04-27 20:06:12 +0200

    Always build PDCurses from scratch
    This avoids issues with doing a 32-bit build after a 64-bit build for
    example.
    
    Piggyback some cleanups.

--- build-wheels.bat
+++ build-wheels.bat
@@ -1,23 +1,43 @@
 @echo off
 
-:: https://en.wikibooks.org/wiki/Windows_Batch_Scripting#Command-line_arguments
-
 if -%1-==-- (
     echo Usage: %0 ^<Python version^> [^<Python version^> ...]
     exit /b 1
 )
 
-echo Building PDCurses
+
+::
+:: Build PDCurses
+::
 
 :: nmake doesn't seem to have an option for switching the working directory
 pushd PDCurses\wincon
+
+:: Always build PDCurses from scratch. This avoids issues with doing a 32-bit
+:: build after a 64-bit build for example.
+
+echo Cleaning PDCurses
+
+nmake -f Makefile.vc clean
+
+echo Building PDCurses
+
+:: nmake doesn't seem to have an option for switching the working directory
 nmake -f Makefile.vc WIDE=y UTF8=y
 if %errorlevel% neq 0 (
     popd
+    echo Check that you're using the Developer Command Prompt
     exit /b %errorlevel%
 )
+
 popd
 
+
+::
+:: Build wheels
+::
+
+:: Process arguments one by one
 :nextarg
 if -%1-==-- goto end
 
