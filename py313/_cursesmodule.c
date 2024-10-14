8d5b3e25c9c9ac2ded874883cabcfa2ee48cbb7c
Paul Barrett <pmbarrett314@gmail.com>
2024-10-13 21:20:26 -0500

    Apply patch 3

--- py313/_cursesmodule.c
+++ py313/_cursesmodule.c
@@ -3554,7 +3554,7 @@ _curses_is_term_resized_impl(PyObject *module, int nlines, int ncols)
 {
     PyCursesInitialised;
 
-    return PyBool_FromLong(is_term_resized(nlines, ncols));
+    return PyBool_FromLong(is_term_resized());
 }
 #endif /* HAVE_CURSES_IS_TERM_RESIZED */
 
